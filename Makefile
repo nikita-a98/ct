7ba954be8e602e445f9b00245a5b849c496253d6
Nikita Lysiuk <nikita.a1998@gmail.com>
2019-02-03 15:21:13 +0200

    testing tests

--- Makefile
+++ Makefile
@@ -14,4 +14,8 @@ ERLC_OPTS += +debug_info +bin_opt_info
 DEPS = cowboy
 dep_cowboy = git https://github.com/ninenines/cowboy.git 2.6.1
 
+TEST_DEPS = $(if $(CI_ERLANG_MK),ci.erlang.mk) ct_helper gun proper
+dep_ct_helper = git https://github.com/extend/ct_helper master
+dep_gun = git https://github.com/ninenines/gun master
+
 include erlang.mk
--- test/test_cowboy_SUITE.erl
+++ test/test_cowboy_SUITE.erl
@@ -1,3 +1,70 @@
+%%-module(test_cowboy_SUITE).
+%%
+%%-compile(export_all).
+%%
+%%-include_lib("common_test/include/ct.hrl").
+%%-include_lib("eunit/include/eunit.hrl").
+%%
+%%
+%%all() ->
+%%  [
+%%    test_case_get
+%%  ].
+%%
+%%init_per_suite(Config) ->
+%%  % действия, выполняемые перед запуском набора тестов
+%%  Config.
+%%
+%%init_per_testcase(_, Config) ->
+%%  ServerPid = start_server(),
+%%  [{server_pid,ServerPid} | Config].
+%%
+%%end_per_testcase(_, _Config) ->
+%%  stop_server().
+%%  % действия, выполняемые после завершения теста
+%%
+%%end_per_suite(Config) ->
+%%  % действия, выполняемые после завершения всего набора тестов
+%%  Config.
+%%
+%%test_case_get(_Config) ->
+%%  1=1.
+%%%ok.
+%%
+%%start_server() ->
+%%  {ok,ServerPid} = test_cowboy_app:start(1,1),
+%%  ServerPid.
+%%
+%%stop_server() ->
+%%  ok = test_cowboy_app:stop(1),
+%%  ok.
+%%
+%%
+%%
+%%-module(test_cowboy_SUITE).
+%%-include_lib("common_test/include/ct.hrl").
+%%
+%%-export([all/0, init_per_testcase/2, end_per_testcase/2]).
+%%-export([ets_tests/1]).
+%%
+%%all() -> [ets_tests].
+%%
+%%init_per_testcase(ets_tests, Config) ->
+%%  TabId = ets:new(account, [ordered_set, public]),
+%%  ets:insert(TabId, {andy, 2131}),
+%%  ets:insert(TabId, {david, 12}),
+%%  ets:insert(TabId, {steve, 12943752}),
+%%  [{table,TabId} | Config].
+%%
+%%end_per_testcase(ets_tests, Config) ->
+%%  ets:delete(?config(table, Config)).
+%%
+%%ets_tests(Config) ->
+%%  TabId = ?config(table, Config),
+%%  [{david, 12}] = ets:lookup(TabId, david),
+%%  steve = ets:last(TabId),
+%%  true = ets:insert(TabId, {zachary, 99}),
+%%  zachary = ets:last(TabId).
 -module(test_cowboy_SUITE).
 
 -compile(export_all).
@@ -5,27 +72,26 @@
 -include_lib("common_test/include/ct.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
+
 all() ->
   [
     test_case_get
   ].
 
-init_per_suite(Config) ->
-  % действия, выполняемые перед запуском набора тестов
-  Config.
 
 init_per_testcase(_, Config) ->
-  % действия, выполняемые перед запуском теста
+  application:start(test_cowboy_app),
   Config.
 
-end_per_testcase(_, Config) ->
+end_per_testcase(_, _Config) ->
+  application:start(test_cowboy_app),
+  ok.
   % действия, выполняемые после завершения теста
-  Config.
-
-end_per_suite(Config) ->
-  % действия, выполняемые после завершения всего набора тестов
-  Config.
 
 test_case_get(_Config) ->
-  os:cmd("curl -i \"http://localhost:8080/?echo=hihi\""),
-ok.
+  1=1.
+%ok.
+
+
+
+
