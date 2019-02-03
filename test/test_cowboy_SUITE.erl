d98b96c30833b00b8010db397e2369fb9d206680
Nikita Lysiuk <nikita.a1998@gmail.com>
2019-02-03 18:16:35 +0200

    edit tests

--- test/test_cowboy_SUITE.erl
+++ test/test_cowboy_SUITE.erl
@@ -1,70 +1,3 @@
-%%-module(test_cowboy_SUITE).
-%%
-%%-compile(export_all).
-%%
-%%-include_lib("common_test/include/ct.hrl").
-%%-include_lib("eunit/include/eunit.hrl").
-%%
-%%
-%%all() ->
-%%  [
-%%    test_case_get
-%%  ].
-%%
-%%init_per_suite(Config) ->
-%%  % действия, выполняемые перед запуском набора тестов
-%%  Config.
-%%
-%%init_per_testcase(_, Config) ->
-%%  ServerPid = start_server(),
-%%  [{server_pid,ServerPid} | Config].
-%%
-%%end_per_testcase(_, _Config) ->
-%%  stop_server().
-%%  % действия, выполняемые после завершения теста
-%%
-%%end_per_suite(Config) ->
-%%  % действия, выполняемые после завершения всего набора тестов
-%%  Config.
-%%
-%%test_case_get(_Config) ->
-%%  1=1.
-%%%ok.
-%%
-%%start_server() ->
-%%  {ok,ServerPid} = test_cowboy_app:start(1,1),
-%%  ServerPid.
-%%
-%%stop_server() ->
-%%  ok = test_cowboy_app:stop(1),
-%%  ok.
-%%
-%%
-%%
-%%-module(test_cowboy_SUITE).
-%%-include_lib("common_test/include/ct.hrl").
-%%
-%%-export([all/0, init_per_testcase/2, end_per_testcase/2]).
-%%-export([ets_tests/1]).
-%%
-%%all() -> [ets_tests].
-%%
-%%init_per_testcase(ets_tests, Config) ->
-%%  TabId = ets:new(account, [ordered_set, public]),
-%%  ets:insert(TabId, {andy, 2131}),
-%%  ets:insert(TabId, {david, 12}),
-%%  ets:insert(TabId, {steve, 12943752}),
-%%  [{table,TabId} | Config].
-%%
-%%end_per_testcase(ets_tests, Config) ->
-%%  ets:delete(?config(table, Config)).
-%%
-%%ets_tests(Config) ->
-%%  TabId = ?config(table, Config),
-%%  [{david, 12}] = ets:lookup(TabId, david),
-%%  steve = ets:last(TabId),
-%%  true = ets:insert(TabId, {zachary, 99}),
-%%  zachary = ets:last(TabId).
 -module(test_cowboy_SUITE).
 
 -compile(export_all).
@@ -75,23 +8,62 @@
 
 all() ->
   [
-    test_case_get
+    echo_get
   ].
 
 
 init_per_testcase(_, Config) ->
   application:start(test_cowboy_app),
+%%  application:start(inets),
+%%  application:start(ssl),
+%%  application:start(cowboy),
   Config.
 
 end_per_testcase(_, _Config) ->
-  application:start(test_cowboy_app),
+  application:stop(test_cowboy_app),
   ok.
-  % действия, выполняемые после завершения теста
 
-test_case_get(_Config) ->
-  1=1.
-%ok.
+%%echo_get(_Config) ->
+%%  {ok,{{_,200,_},_,_}} = httpc:request("http://localhost:8080/?echo=saymyname").
 
 
+echo_get(Config) ->
+  do_echo_get(tcp, http, Config).
 
+do_echo_get(Transport, Protocol, Config) ->
+  {200, _, <<"this is fun">>} = do_get(Transport, Protocol, "/?echo=this+is+fun", [], Config),
+  {400, _, _} = do_get(Transport, Protocol, "/", [], Config),
+  ok.
+
+do_get(Transport, Protocol, Path, ReqHeaders, Config) ->
+  Port = case Transport of
+           tcp -> 8080;
+           ssl -> 8443
+         end,
+  ConnPid = gun_open([{port, Port}, {type, Transport}, {protocol, Protocol}|Config]),
+
+  Ref = gun:get(ConnPid, Path, ReqHeaders),
+  case gun:await(ConnPid, Ref) of
+    {response, nofin, Status, RespHeaders} ->
+      {ok, Body} = gun:await_body(ConnPid, Ref),
+      {Status, RespHeaders, Body};
+    {response, fin, Status, RespHeaders} ->
+      {Status, RespHeaders, <<>>}
+  end.
+
+gun_open(Config) ->
+  gun_open(Config, #{}).
+
+gun_open(Config, Opts) ->
+  {ok, ConnPid} = gun:open("localhost", config(port, Config), Opts#{
+    retry => 0,
+    transport => config(type, Config),
+    transport_opts => proplists:get_value(transport_opts, Config, []),
+    protocols => [config(protocol, Config)]
+  }),
+  ConnPid.
+
+config(Key, Config) ->
+  {_, Value} = lists:keyfind(Key, 1, Config),
+  Value.
 
